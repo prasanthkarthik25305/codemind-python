a9b31f5ad425f8e01620270597eb76ac0ca643bd
Prasanth <126947745+prasanthkarthik25305@users.noreply.github.com>
2023-05-26 14:56:23 +0530

    import math n,m=map(int,input().split()) print(math.gcd(n,m))

--- GCD_or_HCF.py
+++ GCD_or_HCF.py
@@ -1,8 +1,3 @@
-def gcd(a, b):
-    while b != 0:
-        a, b = b, a % b
-    return a
-
-num1,num2 =map(int,input().split())
-result = gcd(num1, num2)
-print(result)
+import math
+n,m=map(int,input().split())
+print(math.gcd(n,m))
