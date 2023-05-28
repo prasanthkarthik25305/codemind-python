0e541b24c728ecb044a3926aa588d4f8076b1b21
Prasanth <126947745+prasanthkarthik25305@users.noreply.github.com>
2023-05-28 08:34:52 +0530

    def isprime(k):     if k<2:         return False     for i in range(2,int(k**0.5)+1):         if k%i==0:             return False     else:         return True n=int(input()) m=int(input()) c=0 for i in range(n,m+1):     if isprime(i):         c+=1 print(c)

--- Primes_in_range.py
+++ Primes_in_range.py
@@ -1,15 +1,16 @@
-n = int(input())
-m = int(input())
-s = 0
-
-for i in range(n, m + 1):
-    c = 0
-    for j in range(2, int(i ** 0.5) + 1):
-        if i % j == 0:
-            c = 1
-            break
+def isprime(k):
+    if k<2:
+        return False
+    for i in range(2,int(k**0.5)+1):
+        if k%i==0:
+            return False
+    else:
+        return True
+n=int(input())
+m=int(input())
+c=0
+for i in range(n,m+1):
+    if isprime(i):
+        c+=1
+print(c)
     
-    if c == 0 and i > 1:
-        s += 1
-
-print(s)
