a65ab6ee6c375b599da42190a3946c2638119ed0
Prasanth <126947745+prasanthkarthik25305@users.noreply.github.com>
2023-05-29 07:51:42 +0530

    n = input() n = ''.join(filter(str.isdigit, n))  # Remove non-digit characters
    c, k = 0, 1
    
    for i in n:
        c += int(i)
        k *= int(i)
    
    if c == k:
        print("Spy Number")
    else:
        print("Not Spy Number")

--- Spy_Number.py
+++ Spy_Number.py
@@ -1,12 +1,13 @@
-n=int(input())
-m=n
-s,k=0,1
-while n!=0:
-    r=n%10
-    s+=r
-    k*=r
-    n=n//10
-if s==k:
-    print('Spy Number')
+n = input()
+n = ''.join(filter(str.isdigit, n))  # Remove non-digit characters
+
+c, k = 0, 1
+
+for i in n:
+    c += int(i)
+    k *= int(i)
+
+if c == k:
+    print("Spy Number")
 else:
-    print('Not Spy Number')
+    print("Not Spy Number")
