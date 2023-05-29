f6f7b2112025986f4348b07176097870e5666852
Prasanth <126947745+prasanthkarthik25305@users.noreply.github.com>
2023-05-29 08:03:57 +0530

    n = int(input()) s = 0
    while n > 0 or s > 9:  # Repeat until we get a single-digit sum
        if n == 0:  # If n becomes 0, update n with the current sum value
            n = s
            s = 0
        else:
            r = n % 10
            s += r
            n = n // 10
    
    print(s)

--- Add_Digits.py
+++ Add_Digits.py
@@ -1,7 +1,13 @@
-n=int(input())
-s=0
-while(n>10):
-    r=n%10
-    n=n//10
-    n=n+r
-print(n)
+n = int(input())
+s = 0
+
+while n > 0 or s > 9:  # Repeat until we get a single-digit sum
+    if n == 0:  # If n becomes 0, update n with the current sum value
+        n = s
+        s = 0
+    else:
+        r = n % 10
+        s += r
+        n = n // 10
+
+print(s)
