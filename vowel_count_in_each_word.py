1c1d38d6300d61a6a4523e46fdb5899c469142c0
Prasanth <126947745+prasanthkarthik25305@users.noreply.github.com>
2023-06-06 18:01:13 +0530

    n=input().lower() m=n.split() v='aeiou' l=[] for i in m:     c=0     for char in i:         if char in v:             c=c+1     l.append(c) for i in l:     print(i,end=' ')

---
+++ vowel_count_in_each_word.py
@@ -0,0 +1,12 @@
+n=input().lower()
+m=n.split()
+v='aeiou'
+l=[]
+for i in m:
+    c=0
+    for char in i:
+        if char in v:
+            c=c+1
+    l.append(c)
+for i in l:
+    print(i,end=' ')
